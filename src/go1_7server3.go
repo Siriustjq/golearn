5152e5303eee536dab8587c38c0f105e6fd711bb
Sirius <370082192@qq.com>
2020-11-27 17:57:16 +0800

    # 1_7创建可以显示gif动画的服务器

---
+++ src/go1_7server3.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var palette1 = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndexs = 0
+	blackIndexs = 1
+)
+
+/**
+这里常量的定义还可以利用常量生成器实现
+var index int
+const (
+	whiteIndex index = iota //iota的初始值是0，逐项加1
+	blackIndex
+)
+*/
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		tjq857(w)
+	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8010", nil))
+}
+func tjq857(out io.Writer) {
+	const (
+		cycle   = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0        //返回一个0-3之间的随机数，rand为随机函数
+	anim := gif.GIF{LoopCount: nframes} //gif.GIF是一个结构体类型，所以{}中间的内容是为结构体中某个或某几个字段赋值
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette1)
+		for t := 0.0; t < cycle*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndexs)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) //这里忽略了编码错误
+}
